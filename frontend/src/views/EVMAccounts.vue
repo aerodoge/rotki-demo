18d29cfc1190d4883558e72ae71d7d4e74ef7c49
aerodoge <m41sense1@gmail.com>
2025-11-14 15:33:34 +0800

    Fix: ui.

--- frontend/src/views/EVMAccounts.vue
+++ frontend/src/views/EVMAccounts.vue
@@ -291,7 +291,7 @@
                       <th
                         class="px-4 py-2 text-left text-xs font-medium text-muted-foreground uppercase"
                       >
-                        WALLET
+                        ADDRESS
                       </th>
                       <th
                         class="px-4 py-2 text-left text-xs font-medium text-muted-foreground uppercase"
